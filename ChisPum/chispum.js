c68d3bfe6ec33635809d5298852715e3f13ff320
Noks ñero <Noks ero>
2023-03-30 21:13:45 -0500

    Ejercicio de chispum, multiplos de 3, de 5 y de 3 y 5.

--- ChisPum/chispum.js
+++ ChisPum/chispum.js
@@ -1,5 +1,9 @@
 function chispum(numero) {
-    return numero;
+    // agregar console.log con numero de retorno
+    return numero % 3 === 0 ? 'chis' :
+        numero % 5 === 0 ? 'pum' :
+        (numero % 3 === 0 && numero % 5 === 0) ? 'chispum' : 
+        null;
 }
 
 module.exports = chispum;
--- ChisPum/chispum.test.js
+++ ChisPum/chispum.test.js
@@ -5,5 +5,23 @@ describe("chispum", () => {
         const esperado = 1;
         const resultado = chispum(1);
         expect(esperado).toBe(resultado);
-    })
+    });
+
+    test("Debería imprimir 'chis' si recibe un múltiplo de 3.", () => {
+        const esperado = "chis";
+        const resultado = chispum(9);
+        expect(esperado).toBe(resultado);
+    });
+
+    test("Debería imprimir 'pum' si recibe un múltiplo de 5.", () => {
+        const esperado = "chis";
+        const resultado = chispum(10);
+        expect(esperado).toBe(resultado);
+    });
+
+    test("Debería imprimir 'chispum' si recibe un múltiplo de 3 y de 5.", () => {
+        const esperado = "chis";
+        const resultado = chispum(15);
+        expect(esperado).toBe(resultado);
+    });
 });
