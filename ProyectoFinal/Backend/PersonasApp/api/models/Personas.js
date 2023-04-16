7e7f8d4594ce25ab5978aa9841250b0417f3114f
MikeDarkz <gualterosmichael@hotmail.com>
2023-04-16 12:34:43 -0500

    Se crea backend en Sails JS.

---
+++ ProyectoFinal/Backend/PersonasApp/api/models/Personas.js
@@ -0,0 +1,18 @@
+/**
+ * Personas.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    nombre: { type: 'string', required: true },
+    apellido: { type: 'string', required: true },
+    edad: { type: 'number', required: false },
+    genero: { type: 'string', required: false },
+  },
+
+};
+
